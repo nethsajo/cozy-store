82be4cee029200d3edc62888d0f922e498bc0857
Joy Reyes <nethsajo98@gmail.com>
2022-03-31 11:08:15 +0800

    style layout and breadcrumb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+let plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./public/**/*.{html,js}', './src/**/*.{html,js}'],
   theme: {
@@ -6,12 +8,16 @@ module.exports = {
         xs: { max: '320px' },
       },
       dropShadow: {
-        customOne: '0 8px 12px rgba(0,0,0,0.15)',
+        customSm: '0 8px 12px rgba(0,0,0,0.15)',
       },
       boxShadow: {
         customSm: '0 4px 18px rgba(32,32,32,0.05)',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('slash', '.breadcrumb:not(:first-child):before');
+    }),
+  ],
 };
